c82e8efd7c35287991aa672b0a4e3fda5d78d86d
Eva Anderson <evajoyanderson@gmail.com>
2021-10-13 14:57:19 +1300

    Quit key initiated

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pygame
+from pygame.locals import (
+    K_ESCAPE,
+    QUIT,
+    KEYDOWN
+)
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
@@ -11,6 +16,9 @@ loop = True
 running = True
 while running:
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
         screen.fill((226,226,226))
         pygame.display.flip()
 
