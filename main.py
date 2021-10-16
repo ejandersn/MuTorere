2b279dcbb9522c44f0a48342c700e97f724307fd
Eva Anderson <evajoyanderson@gmail.com>
2021-10-16 20:46:48 +1300

    Integer Text Box

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
-from pygame.constants import K_BACKSPACE
 from pygame.locals import (
     K_ESCAPE,
     QUIT,
     KEYDOWN,
-    K_BACKSPACE
+    K_BACKSPACE,
+    K_RETURN,
+    K_0,
+    K_1,
+    K_2,
+    K_3,
+    K_4,
+    K_5,
+    K_6,
+    K_7,
+    K_8,
+    K_9
+
 )
 
 SCREEN_WIDTH = 800
@@ -20,7 +31,9 @@ pygame.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 
 base_font = pygame.font.Font(None,32)
-user_text = ''
+user_text = int()
+input_rect = pygame.Rect(100,700,50,32)
+color = (0,0,0)
 
 running = True
 while running:
@@ -29,14 +42,49 @@ while running:
             if event.key == pygame.K_BACKSPACE:
                  user_text = user_text[:-1]
             else:
-                user_text += event.unicode
+                if event.key == pygame.K_RETURN:
+                    if user_text > 9:
+                        print('ERROR!')
+                else:
+                    if event.key == pygame.K_0:
+                        user_text += (0)
+                    else:
+                        if event.key == K_1:
+                            user_text += (1)
+                        else: 
+                            if event.key == K_2:
+                                user_text += (2)
+                            else:
+                                if event.key == K_3:
+                                    user_text += (3)
+                                else:
+                                    if event.key == K_4:
+                                        user_text += (4)
+                                    else:
+                                        if event.key == K_5:
+                                            user_text += (5)
+                                        else:
+                                            if event.key == K_6:
+                                                user_text += (6)
+                                            else:
+                                                if event.key == K_7:
+                                                    user_text += (7)
+                                                else:
+                                                    if event.key == K_8:
+                                                        user_text += (8)
+                                                    else:
+                                                        if event.key == K_9:
+                                                            user_text += (9)
+
             if event.key ==  K_ESCAPE:
                 running = False
-            
         screen.fill((255,255,255))
         screen.blit(greenPerepereOne, (x,y))
-        text_surface = base_font.render(user_text,True,(0,0,0))
-        screen.blit(text_surface,(100,100))
+        pygame.draw.rect(screen,color,input_rect,2 )
+        this = str(user_text)
+
+        text_surface = base_font.render(this,True,(0,0,0))
+        screen.blit(text_surface,(input_rect.x + 10,input_rect.y + 5))
         pygame.display.flip()
 
 pygame.quit()
