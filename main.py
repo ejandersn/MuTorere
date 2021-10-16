14c1cd7faa02bb16a7f9751b297fcd6d2ac26998
Eva Anderson <evajoyanderson@gmail.com>
2021-10-16 18:12:43 +1300

    Added first perepere

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ from pygame.locals import (
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 
+x = 50
+y = 100
+
+greenPerepereOne = pygame.image.load('piece_one.png')
+
 pygame.init()
 
 screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 
-loop = True
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == K_ESCAPE:
+            if event.key ==  K_ESCAPE:
                 running = False
-        screen.fill((226,226,226))
+        screen.fill((255,255,255))
+        screen.blit(greenPerepereOne, (x,y))
         pygame.display.flip()
 
 pygame.quit()
