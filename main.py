754fd89de4f5a23c52c5bbc97839a78e9539967c
Eva Anderson <evajoyanderson@gmail.com>
2021-10-17 00:58:20 +1300

    Green Perepere Included

--- main.py
+++ main.py
@@ -21,13 +21,24 @@ from pygame.locals import (
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 
-x = 370
+x = 70
 y = 100
 
+board = pygame.image.load('guidestar.png')
+gp_one = pygame.image.load('realgreen.png')
+gp_two = pygame.image.load('realgreentwo.png')
+gp_three = pygame.image.load('realgreenthree.png')
+gp_four = pygame.image.load('realgreenfour.png')
 
-
-board = pygame.image.load('betterstar.png')
-greenPerepereOne = pygame.image.load('piece_one.png')
+pos_one = 360,100
+pos_two = 530,165
+pos_three = 610,330
+pos_four = 535,510
+pos_five = 360,y
+pos_six = 175,y
+pos_seven = x,330
+pos_eight = 175,155
+pos_nine = x,y
 
 pygame.init()
 
@@ -75,17 +86,21 @@ while running:
                                                     else:
                                                         if event.key == K_9:
                                                             user_text = (9)
-                                                            
-        if event.key ==  K_ESCAPE:
-            running = False
+                                                        else:    
+                                                            if event.key ==  K_ESCAPE:
+                                                                running = False
         screen.fill((255,255,255))
-        
+
         pygame.draw.rect(screen,color,input_rect,2 )
         user_string = str(user_text)
-        screen.blit(board,(125,100))
-        screen.blit(greenPerepereOne, (x,y))
         text_surface = base_font.render(user_string,True,(0,0,0))
         screen.blit(text_surface,(input_rect.x + 10,input_rect.y + 5))
+
+        screen.blit(board,(125,100))
+        screen.blit(gp_one, (pos_one))
+        screen.blit(gp_two,(pos_two))
+        screen.blit(gp_three,(pos_three))
+        screen.blit(gp_four,(pos_four))
         pygame.display.flip()
 
 pygame.quit()
