738319b434f376d5c1aa283f12db00da2fdef7fb
Eva Anderson <evajoyanderson@gmail.com>
2021-10-16 19:04:16 +1300

    Basic Text Ability

--- main.py
+++ main.py
@@ -17,14 +17,20 @@ pygame.init()
 
 screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
 
+base_font = pygame.font.Font(None,32)
+user_text = ''
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
+            user_text += event.unicode
             if event.key ==  K_ESCAPE:
                 running = False
         screen.fill((255,255,255))
         screen.blit(greenPerepereOne, (x,y))
+        text_surface = base_font.render(user_text,True,(0,0,0))
+        screen.blit(text_surface,(100,100))
         pygame.display.flip()
 
 pygame.quit()
